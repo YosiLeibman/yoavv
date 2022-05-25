349aba34693d277d896e37b21ffe4cd6585b77ee
Your Name <you@example.com>
2022-05-25 15:17:01 +0300

    react build

--- VacationServer/app.js
+++ VacationServer/app.js
@@ -6,10 +6,11 @@ const server = express();
 const vacationController = require('./controllers/vacations-controllers');
 const usersController = require('./controllers/users-controllers');
 
-server.use(cors({ origin: 'http://localhost:3000'}));
+server.use(express.static('build'));
 server.use(express.json());
 
 server.use('/vacations', vacationController);
 server.use('/users', usersController);
+server.get('/', (req,res)=>res.sendFile(__dirname+'/build/index.html'));
 
 server.listen(3001, () => console.log('Listening on http://localhost:3001'));
